ee876ca0a3ef8d96083ef9c3e97b358d2a3aaa3c
Yue Chen <chenchristin@outlook.com>
2023-02-06 22:24:54 +0100

    3

---
+++ src/simple.cpp
@@ -0,0 +1,44 @@
+
+
+#include <nori/integrator.h>
+#include <nori/scene.h>
+
+NORI_NAMESPACE_BEGIN
+
+    class SimpleIntegrator : public Integrator {
+    private:
+        Point3f m_position;
+        Color3f m_energy;
+    public:
+
+        SimpleIntegrator(const PropertyList &propList) {
+            m_position = propList.getPoint("position", Point3f());
+            m_energy = propList.getColor("energy", Color3f());
+        }
+
+        Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+            Intersection its;//相交
+            if (!scene->rayIntersect(ray, its))
+                return {0.0f};
+
+            Vector3f L = m_position - its.p;//光源方向
+
+            if (scene->rayIntersect(Ray3f(its.p + L * Epsilon, L)))//可见性
+                return {0.0f};
+
+            //直接光照：Phi/4pi*pi * cos_theta/||x-p||^2 * V(x<->p)
+            return 0.25f * INV_PI * INV_PI * m_energy * std::max(0.0f, its.shFrame.n.dot(L.normalized())) / L.dot(L);
+        }
+
+        std::string toString() const {
+            return tfm::format(
+                    "SimpleIntegrator[\n"
+                    "  position = %s\n"
+                    "  energy = %s\n"
+                    "]", m_position.toString(), m_energy.toString());
+        }
+    };
+
+    NORI_REGISTER_CLASS(SimpleIntegrator, "simple");
+
+NORI_NAMESPACE_END
--- src/warp.cpp
+++ src/warp.cpp
@@ -1,69 +1,143 @@
-/*
-    This file is part of Nori, a simple educational ray tracer
-
-    Copyright (c) 2015 by Wenzel Jakob
-*/
-
 #include <nori/warp.h>
 #include <nori/vector.h>
 #include <nori/frame.h>
 
+
 NORI_NAMESPACE_BEGIN
 
-Point2f Warp::squareToUniformSquare(const Point2f &sample) {
-    return sample;
-}
+    Point2f Warp::squareToUniformSquare(const Point2f &sample) {
+        return sample;
+    }
 
-float Warp::squareToUniformSquarePdf(const Point2f &sample) {
-    return ((sample.array() >= 0).all() && (sample.array() <= 1).all()) ? 1.0f : 0.0f;
-}
+    float Warp::squareToUniformSquarePdf(const Point2f &sample) {
+        return ((sample.array() >= 0).all() && (sample.array() <= 1).all()) ? 1.0f : 0.0f;
+    }
 
-Point2f Warp::squareToTent(const Point2f &sample) {
-    throw NoriException("Warp::squareToTent() is not yet implemented!");
-}
+    Point2f Warp::squareToTent(const Point2f &sample) {
+        Point2f returnPoint;
+        for(int i=0;i<2;i++) {
+            if(sample[i] < 0.5f)
+                returnPoint[i] = sqrt(2*sample[i]) - 1;
+            else
+                returnPoint[i] = 1 - sqrt(2-2*sample[i]);
+        }
 
-float Warp::squareToTentPdf(const Point2f &p) {
-    throw NoriException("Warp::squareToTentPdf() is not yet implemented!");
-}
+        return returnPoint;
+    }
 
-Point2f Warp::squareToUniformDisk(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformDisk() is not yet implemented!");
-}
+    float Warp::squareToTentPdf(const Point2f &p) {
+        float t[2];
+        for(int i=0;i<2;i++) {
+            if(p[i] <= 1 && p[i] >= -1)
+                t[i] = p[i];
+            else
+                t[i] = 0;
+        }
+        return (1-abs(t[0])) * (1-abs(t[1]));
+    }
 
-float Warp::squareToUniformDiskPdf(const Point2f &p) {
-    throw NoriException("Warp::squareToUniformDiskPdf() is not yet implemented!");
-}
+    Point2f Warp::squareToUniformDisk(const Point2f &sample) {
+        return Point2f(sqrt(sample[0]) * cos(2*M_PI*sample[1]), sqrt(sample[0]) * sin(2*M_PI*sample[1]));
+    }
 
-Vector3f Warp::squareToUniformSphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformSphere() is not yet implemented!");
-}
+    float Warp::squareToUniformDiskPdf(const Point2f &p) {
+        if(sqrt(p[0] * p[0] + p[1] * p[1]) > 1) {
+            return 0;
+        }
+        else
+            return 1.f/M_PI;
+    }
 
-float Warp::squareToUniformSpherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformSpherePdf() is not yet implemented!");
-}
+    Vector3f Warp::squareToUniformSphere(const Point2f &sample) {
+        double theta = 2 * M_PI * sample[0];
+        double phi = acos(1 - 2 * sample[1]);
 
-Vector3f Warp::squareToUniformHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformHemisphere() is not yet implemented!");
-}
+        return Vector3f(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
+    }
 
-float Warp::squareToUniformHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformHemispherePdf() is not yet implemented!");
-}
+    float Warp::squareToUniformSpherePdf(const Vector3f &v) {
+        if(sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]) > 1)
+            return 0.f;
 
-Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToCosineHemisphere() is not yet implemented!");
-}
+        return 1.f/(4*M_PI);
+    }
 
-float Warp::squareToCosineHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToCosineHemispherePdf() is not yet implemented!");
-}
+    Vector3f Warp::squareToUniformHemisphere(const Point2f &sample) {
+        double theta = 2 * M_PI * sample[0];
+        double phi = acos(1 - sample[1]);
 
-Vector3f Warp::squareToBeckmann(const Point2f &sample, float alpha) {
-    throw NoriException("Warp::squareToBeckmann() is not yet implemented!");
-}
+        return Vector3f(sin(phi) * cos(theta), sin(phi) * sin(theta), cos(phi));
+    }
 
-float Warp::squareToBeckmannPdf(const Vector3f &m, float alpha) {
-    throw NoriException("Warp::squareToBeckmannPdf() is not yet implemented!");
-}
+    float Warp::squareToUniformHemispherePdf(const Vector3f &v) {
+        if(sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]) > 1 || v[2] < 0)
+            return 0.f;
+
+        return 1.f/(2*M_PI);
+    }
+
+    Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
+        Point2f d = squareToUniformDisk(sample);
+        float z = sqrt(std::max(0.f, 1 - d.x() * d.x() - d.y() * d.y()));
+        return Vector3f(d.x(),d.y(),z);
+    }
+
+    float Warp::squareToCosineHemispherePdf(const Vector3f &v) {
+        if(v[2] < 0)
+            return 0.f;
+
+        return v[2] / M_PI;
+    }
+
+    Vector3f Warp::squareToBeckmann(const Point2f &sample, float alpha) {
+        float phi = 2*M_PI*sample[0];
+        float invAlpha2 = 1.f/alpha; invAlpha2 *= invAlpha2;
+        float theta = acos(sqrt(1/(1-alpha*alpha*log(sample[1]))));
+
+        return Vector3f(sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta));
+    }
+
+    float Warp::squareToBeckmannPdf(const Vector3f &m, float alpha) {
+        float cosTheta = Frame::cosTheta(m);
+        float tanTheta = Frame::tanTheta(m);
+
+        if(cosTheta <= 0)
+            return 0.f;
+
+        float azimuthal = 0.5 * INV_PI;
+        float longitudinal = 2*exp((-tanTheta*tanTheta)/(alpha*alpha))/(alpha*alpha*pow(cosTheta,3));
+
+        return azimuthal * longitudinal;
+    }
+
+//    Vector3f Warp::squareToGTR1(const Point2f & sample, float alpha)
+//    {
+//        float phi = 2 * M_PI * sample[0];
+//        float theta = 0;
+//        if (alpha < 1)
+//            theta = acos(sqrt((1 - pow(pow(alpha, 2), 1 - sample[1])) / (1 - pow(alpha, 2))));
+//        return Vector3f(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+//    }
+//
+//    float Warp::squareToGTR1Pdf(const Vector3f & m, float alpha)
+//    {
+//        float cosTheta = m.z() / m.norm();
+//        if (alpha >= 1)
+//            return INV_PI;
+//        return (abs(m.norm() - 1) < Epsilon && m.z() >= 0) * cosTheta * (pow(alpha, 2) - 1)*INV_PI/(2*std::log(alpha) * (1 + pow(cosTheta, 2) * (pow(alpha, 2) - 1)));
+//    }
+//
+//    Vector3f Warp::squareToGTR2(const Point2f & sample, float alpha)
+//    {
+//        float phi = 2 * M_PI * sample[0];
+//        float theta = acos(sqrt((1 - sample[1]) / (1 + (pow(alpha, 2) - 1) * sample[1])));
+//        return Vector3f(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+//    }
+//
+//    float Warp::squareToGTR2Pdf(const Vector3f & m, float alpha)
+//    {
+//        float cosTheta = m.z() / m.norm();
+//        return (abs(m.norm() - 1) < Epsilon && m.z() >= 0) * pow(alpha, 2) * cosTheta * INV_PI / pow(1 + pow(cosTheta, 2) * (pow(alpha, 2) - 1), 2);
+//    }
 
 NORI_NAMESPACE_END
